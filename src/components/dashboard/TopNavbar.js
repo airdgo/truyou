f7c96a28215aee8a840a387481923c1bbf762c7f
Vlad Dragoi <vladdragoi1@gmail.com>
2022-03-30 08:01:20 +0300

    top navbar styling

--- src/components/dashboard/TopNavbar.js
+++ src/components/dashboard/TopNavbar.js
@@ -1,15 +1,32 @@
 import { SearchBar } from "./SearchBar";
 import { useAuth } from "../authentication/AuthProvider";
+import { RiBitCoinLine } from "react-icons/ri";
+import { FiBell } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
 
 export const TopNavbar = () => {
 	const { currentUser } = useAuth();
 
 	return (
-		<nav className="w-full absolute bg-primary flex justify-between py-3 pl-20">
+		<nav className="w-full absolute bg-primary flex justify-between items-center py-3 pl-20">
 			<SearchBar />
-			<ul className="text-white">
-				<li>Hello, {currentUser.attributes.given_name}</li>
-			</ul>
+			<div className="flex">
+				<div className="text-search text-white">
+					<span className=" font-bold">Hello, </span>
+					{currentUser.attributes.given_name}
+				</div>
+				<ul className="text-white flex text-xl gap-4 mx-20">
+					<li>
+						<RiBitCoinLine />
+					</li>
+					<li>
+						<FiBell />
+					</li>
+					<li>
+						<FiMail />
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
