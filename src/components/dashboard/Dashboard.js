cd619405385c0d683f68577671028cb3db280493
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-01 10:22:59 +0300

    created moods and posts layout

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,13 @@
 import { LeftNavbar } from "./LeftNavbar";
+import { MoodAndPosts } from "./MoodAndPosts";
 import { TopNavbar } from "./TopNavbar";
 
 export const Dashboard = () => {
 	return (
 		<div className="bg-background min-h-screen grid grid-cols-[4rem_auto_15rem] grid-rows-[5rem_1fr]">
 			<LeftNavbar />
-			{/* <div className=" bg-red-500 row-span-full">sidebard</div> */}
 			<TopNavbar />
-			{/* <div className=" bg-slate-500 col-span-2">navbar</div> */}
-			<div className="bg-orange-500">changing</div>
+			<MoodAndPosts />
 			<div className="bg-purple-500 row-span-2">buckedlist</div>
 		</div>
 	);
---
+++ src/components/dashboard/MoodAndPosts.js
@@ -0,0 +1,8 @@
+export const MoodAndPosts = () => {
+	return (
+		<div className="grid grid-rows-[1fr_2fr] gap-4 m-4">
+			<div className="bg-green-400 rounded-md">Moods</div>
+			<div className="bg-green-800 rounded-md">Posts</div>
+		</div>
+	);
+};
