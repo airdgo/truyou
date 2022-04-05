8e74eafdd7c7ff505eb953efebf041030b853a1f
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-05 09:03:38 +0300

    posts header styling

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,7 +1,7 @@
 export const PrimaryButton = ({ children, bgColor = "bg-primary" }) => {
 	return (
 		<button
-			className={`focus:shadow-outline cursor-pointer rounded-md ${bgColor} py-2 px-4 font-light text-white focus:outline-none lg:mb-4 lg:text-sm`}
+			className={`focus:shadow-outline cursor-pointer rounded-md ${bgColor} py-1 px-4 font-light text-white focus:outline-none lg:py-2 lg:text-sm`}
 		>
 			{children}
 		</button>
--- src/components/dashboard/Posts.js
+++ src/components/dashboard/Posts.js
@@ -1,17 +1,29 @@
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { PrimaryButton } from "../PrimaryButton";
+import { FiLayers } from "react-icons/fi";
+import { BiGridAlt } from "react-icons/bi";
 
 export const Posts = () => {
 	return (
-		<div className="bg-green-800 rounded-md p-5 font-card font-normal">
-			<div>
+		<div className="bg-white rounded-md p-5 font-card font-normal shadow-xl">
+			<div className="flex flex-col gap-8">
 				<div className="text-neutralDark flex justify-between items-center">
 					<div className="text-xs">Posts</div>
-					<HiOutlineDotsHorizontal className="text-xl" />
+					<HiOutlineDotsHorizontal className="text-xl cursor-pointer" />
 				</div>
-				<div>
-					<PrimaryButton>Add new post</PrimaryButton>
-					<PrimaryButton>Add new friend</PrimaryButton> icons
+				<div className="flex items-center justify-between">
+					<div className="flex gap-4">
+						<PrimaryButton bgColor="bg-accent">Add new post</PrimaryButton>
+						<PrimaryButton bgColor="bg-accent">Add new friend</PrimaryButton>
+					</div>
+					<div className="flex text-xl text-neutralDark gap-3">
+						<div className="bg-neutral p-1 rounded-md cursor-pointer">
+							<BiGridAlt />
+						</div>
+						<div className="bg-neutral p-1 rounded-md cursor-pointer">
+							<FiLayers />
+						</div>
+					</div>
 				</div>
 			</div>
 			<div>Posts content</div>
