8ac76e75eeda934c99072761c011e3ad40491d96
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-08 08:17:53 +0300

    smaller top navbar

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { TopNavbar } from "./TopNavbar";
 
 export const Dashboard = () => {
 	return (
-		<div className="bg-background min-h-screen grid grid-cols-[4rem_auto_15rem] grid-rows-[5rem_1fr]">
+		<div className="bg-background min-h-screen grid grid-cols-[4rem_auto_15rem] grid-rows-[4rem_1fr]">
 			<LeftNavbar />
 			<TopNavbar />
 			<MoodAndPosts />
--- src/components/dashboard/TopNavbar.js
+++ src/components/dashboard/TopNavbar.js
@@ -8,7 +8,7 @@ export const TopNavbar = () => {
 	const { currentUser } = useAuth();
 
 	return (
-		<nav className="w-full bg-primary flex justify-between items-center py-3 pl-4 col-span-2">
+		<nav className="w-full bg-primary flex justify-between items-center pl-4 col-span-2">
 			<SearchBar />
 			<div className="flex items-center">
 				<div className="text-search text-white">
