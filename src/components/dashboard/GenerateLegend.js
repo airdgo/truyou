3621aeee3ea0bda76ffab3ae14555894d5c29c42
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-13 09:35:31 +0300

    added keys to list items

--- src/components/dashboard/GenerateLegend.js
+++ src/components/dashboard/GenerateLegend.js
@@ -3,7 +3,7 @@ export const GenerateLegend = ({ data }) => {
 		<ul className="text-neutralDark text-xs font-light">
 			{data.labels.map((label, index) => {
 				return (
-					<li className="flex items-center">
+					<li key={index} className="flex items-center">
 						<span
 							className={`w-2 h-2 rounded-full mr-2 bg-[${data.datasets[0].backgroundColor[index]}]`}
 						></span>
