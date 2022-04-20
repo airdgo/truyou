08f94a6665ab1817e7e5b01eabaa8a33f20e6379
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-20 08:45:35 +0300

    changed svg icons attributes

--- src/icons/FriendsStatusIcon.js
+++ src/icons/FriendsStatusIcon.js
@@ -10,38 +10,38 @@ export const FriendsStatusIcon = () => {
 			<path
 				d="M11.5 13.8003H5C2.8 13.8003 1 15.6003 1 17.8003V19.8003"
 				stroke="#150052"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M8.5 9.80029C10.7091 9.80029 12.5 8.00943 12.5 5.80029C12.5 3.59115 10.7091 1.80029 8.5 1.80029C6.29086 1.80029 4.5 3.59115 4.5 5.80029C4.5 8.00943 6.29086 9.80029 8.5 9.80029Z"
 				stroke="#150052"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<g clip-path="url(#clip0_115_2269)">
 				<path
 					d="M11.667 12.8005V23.4671L16.3337 20.8005L21.667 23.4671L26.3337 20.8005V10.1338L21.667 12.8005L16.3337 10.1338L11.667 12.8005Z"
 					stroke="#150052"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M16.333 10.1338V20.8005"
 					stroke="#150052"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M21.667 12.8003V23.467"
 					stroke="#150052"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 			</g>
 			<defs>
