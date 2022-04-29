0e5f07108e25403bc152e6e141c9e38a0772cb3b
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-29 08:54:49 +0300

    deleted imports

--- src/components/Bucketlist/Bucketlist.js
+++ src/components/Bucketlist/Bucketlist.js
@@ -1,6 +1,3 @@
-import { Section } from "../dashboard/Section";
-import { AiOutlinePlus } from "react-icons/ai";
-import { PrimaryButton } from "../PrimaryButton";
 import { useSidebar } from "../dashboard/Dashboard";
 import { CompressedBucketlist } from "../Bucketlist/CompressedBucketlist";
 import { ExtendedBucketlist } from "../Bucketlist/ExtendedBucketlist";
