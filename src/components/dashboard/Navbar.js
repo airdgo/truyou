ec3dbde8a917d564ba078d10bc2d1ed8290a7a1c
Vlad Dragoi <vladdragoi1@gmail.com>
2022-04-29 08:59:48 +0300

    navbar translates when sidebar extends

--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -3,12 +3,19 @@ import { useAuth } from "../authentication/AuthProvider";
 import { CryptoIcon } from "../../icons/SidebarIcons/CryptoIcon";
 import { BellIcon } from "../../icons/NavbarIcons/BellIcon";
 import { MessageIcon } from "../../icons/NavbarIcons/MessageIcon";
+import { useSidebar } from "./Dashboard";
 
 export const Navbar = () => {
 	const { currentUser } = useAuth();
+	const { sidebarExtended } = useSidebar();
+
+	const padding = sidebarExtended ? "pl-60" : "pl-20";
+	const className =
+		"w-full bg-primary flex justify-between items-center fixed min-h-[4rem] top-0 left-0 " +
+		padding;
 
 	return (
-		<nav className="w-full bg-primary flex justify-between items-center pl-20 fixed min-h-[4rem] top-0 left-0">
+		<nav className={className}>
 			<SearchBar />
 			<div className="flex items-center">
 				<div className="text-search text-white">
