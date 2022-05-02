97e66f6ec610c9187e54db0bd98c3e0606955b2f
Vlad Dragoi <vladdragoi1@gmail.com>
2022-05-02 08:26:48 +0300

    modal close when backdrop is clicked

--- src/components/Newpost/Backdrop.js
+++ src/components/Newpost/Backdrop.js
@@ -1,6 +1,9 @@
 export const Backdrop = ({ children, onClick }) => {
 	return (
-		<div className="absolute top-0 left-0 h-full w-full bg-[#000000e1] flex items-center justify-center">
+		<div
+			onClick={onClick}
+			className="absolute top-0 left-0 h-full w-full bg-[#000000e1] flex items-center justify-center"
+		>
 			{children}
 		</div>
 	);
