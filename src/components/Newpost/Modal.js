a30f38e74d71e1b9efc65d313e4b54725ef175d5
Vlad Dragoi <vladdragoi1@gmail.com>
2022-05-07 16:08:09 +0300

    the use can select only one mood

--- src/components/Newpost/Modal.js
+++ src/components/Newpost/Modal.js
@@ -2,18 +2,62 @@ import { Backdrop } from "./Backdrop";
 import { createPortal } from "react-dom";
 import { useAuth } from "../authentication/AuthProvider";
 import { NextStepIcon } from "../../icons/MoodsAndPostsIcons/NextStepIcon";
+import { useState } from "react";
 
 export const Modal = ({ handleClose, modalOpen }) => {
 	const { currentUser } = useAuth();
 	const userName = currentUser.attributes.given_name;
 
-	const moods = ["happy", "sad", "angry", "scared", "surprised", "disgusted"];
+	const [moods, setMoods] = useState([
+		{
+			id: 0,
+			mood: "happy",
+			isSelected: false,
+		},
+		{
+			id: 1,
+			mood: "sad",
+			isSelected: false,
+		},
+		{
+			id: 2,
+			mood: "angry",
+			isSelected: false,
+		},
+		{
+			id: 3,
+			mood: "scared",
+			isSelected: false,
+		},
+		{
+			id: 4,
+			mood: "surprised",
+			isSelected: false,
+		},
+		{
+			id: 5,
+			mood: "disgusted",
+			isSelected: false,
+		},
+	]);
+
+	const handleSelectedMood = (id) => {
+		const newMoods = moods.map((mood) => {
+			return mood.id === id
+				? { ...mood, isSelected: !mood.isSelected }
+				: mood.isSelected
+				? { ...mood, isSelected: !mood.isSelected }
+				: mood;
+		});
+
+		setMoods(newMoods);
+	};
 
 	return createPortal(
 		<Backdrop onClick={handleClose}>
 			<div
 				onClick={(e) => e.stopPropagation()}
-				className=" relative w-full max-w-6xl bg-white py-24 px-12 font-card text-primary"
+				className="relative w-full max-w-6xl rounded bg-white py-24 px-12 font-card text-primary"
 			>
 				<div className="mb-12">
 					<h1 className="text-4xl font-semibold">{userName},</h1>
@@ -21,13 +65,20 @@ export const Modal = ({ handleClose, modalOpen }) => {
 				</div>
 				<p className="my-8">1. Please, choose your mood</p>
 				<ul className="flex gap-8">
-					{moods.map((mood, index) => {
+					{moods.map((mood) => {
+						const selectedStyle = mood.isSelected
+							? "bg-accent text-white"
+							: "bg-background";
+						const className =
+							"cursor-pointer rounded px-4 py-2 " + selectedStyle;
+
 						return (
 							<li
-								key={index}
-								className="cursor-pointer rounded bg-background px-4 py-2"
+								key={mood.id}
+								className={className}
+								onClick={() => handleSelectedMood(mood.id)}
 							>
-								{mood}
+								{mood.mood}
 							</li>
 						);
 					})}
