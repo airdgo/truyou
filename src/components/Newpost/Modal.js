02377d342fe7ed159c8ce2067fd0107587d51855
Vlad Dragoi <vladdragoi1@gmail.com>
2022-05-08 08:22:21 +0300

    created step 1 component

--- src/components/Newpost/Modal.js
+++ src/components/Newpost/Modal.js
@@ -1,32 +1,11 @@
 import { Backdrop } from "./Backdrop";
-import { ModalContainer } from "./ModalContainer";
-import { ChooseMood } from "./ChooseMood";
+import { Step1 } from "./Step1";
 import { createPortal } from "react-dom";
-import { useAuth } from "../authentication/AuthProvider";
-import { NextStepIcon } from "../../icons/MoodsAndPostsIcons/NextStepIcon";
 
 export const Modal = ({ handleClose, modalOpen }) => {
-	const { currentUser } = useAuth();
-	const userName = currentUser.attributes.given_name;
-
 	return createPortal(
 		<Backdrop onClick={handleClose}>
-			<ModalContainer>
-				<div className="mb-12">
-					<h1 className="text-4xl font-semibold">{userName},</h1>
-					<p className="mt-2">How are you feeling today?</p>
-				</div>
-
-				<p className="my-8">1. Please, choose your mood</p>
-
-				<ChooseMood />
-
-				<button className="absolute right-16 top-10" onClick={handleClose}>
-					Close
-				</button>
-
-				<NextStepIcon className="cursor-pointer fill-neutral stroke-neutralDark hover:fill-accent hover:stroke-white" />
-			</ModalContainer>
+			<Step1 handleClose={handleClose} />
 		</Backdrop>,
 		document.getElementById("portal")
 	);
---
+++ src/components/Newpost/Step1.js
@@ -0,0 +1,28 @@
+import { ModalContainer } from "./ModalContainer";
+import { ChooseMood } from "./ChooseMood";
+import { NextStepIcon } from "../../icons/MoodsAndPostsIcons/NextStepIcon";
+import { useAuth } from "../authentication/AuthProvider";
+
+export const Step1 = ({ handleClose }) => {
+	const { currentUser } = useAuth();
+	const userName = currentUser.attributes.given_name;
+
+	return (
+		<ModalContainer>
+			<div className="mb-12">
+				<h1 className="text-4xl font-semibold">{userName},</h1>
+				<p className="mt-2">How are you feeling today?</p>
+			</div>
+
+			<p className="my-8">1. Please, choose your mood</p>
+
+			<ChooseMood />
+
+			<button className="absolute right-16 top-10" onClick={handleClose}>
+				Close
+			</button>
+
+			<NextStepIcon className="cursor-pointer fill-neutral stroke-neutralDark hover:fill-accent hover:stroke-white" />
+		</ModalContainer>
+	);
+};
